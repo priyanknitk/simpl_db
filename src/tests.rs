8694680995b0baa07fa28299cd8e750af3f17bb2
Priyank Mandavawala <primanda@microsoft.com>
2024-01-28 11:17:59 +0530

    More tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::table::Table;
+    use crate::{enums::ExecuteResult, executor, row::Row, statement::{self, Statement, StatementType}, table::Table};
  
     // Table tests
     #[test]
@@ -17,5 +17,52 @@ mod tests {
         assert_eq!(cursor.cell_num, 0, "cell_num should be 0.");
         assert_eq!(cursor.page_num, 0, "page_num should be 0.");
         assert_eq!(cursor.end_of_table, false, "end_of_table should be false.");
+    }
+
+    #[test]
+    fn table_start() {
+        let mut table = Table::open("test.db");
+        let cursor = table.table_start();
+        assert_eq!(cursor.cell_num, 0, "cell_num should be 0.");
+        assert_eq!(cursor.page_num, 0, "page_num should be 0.");
+        assert_eq!(cursor.end_of_table, true, "end_of_table should be false.");
+    }
+
+    #[test]
+    fn test_insert_row() {
+        let (_, execute_result) =  insert_row();
+        match execute_result {
+            ExecuteResult::ExecuteSuccess => println!("Executed."),
+            _ => panic!("Error executing statement.")
+        }
+    }
+
+    #[test]
+    fn select_test_row() {
+        let (mut table, _) =  insert_row();
+        let statement = Statement{
+            row_to_insert: None,
+            statement_type: StatementType::StatementSelect
+        };
+        let execute_result = executor::execute_statement(&statement, &mut table);
+        match execute_result {
+            ExecuteResult::ExecuteSuccess => println!("Executed."),
+            _ => panic!("Error executing statement.")
+        }
+    }
+
+    fn insert_row() -> (Table, ExecuteResult) {
+        let mut table = Table::open("test.db");
+        let row_to_insert = Row{
+            id: 1,
+            username: "test".to_string(),
+            email: "test@test.com".to_string()
+        };
+        let statement = Statement{
+            row_to_insert: Some(row_to_insert),
+            statement_type: StatementType::StatementInsert
+        };
+        let execute_result = executor::execute_statement(&statement, &mut table);
+        (table, execute_result)
     }
 }
